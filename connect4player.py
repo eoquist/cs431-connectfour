3737ac00d4c5932fcc15b6f38c48d8cf3aa90816
Emilee Oquist <emiwemi495@gmail.com>
2023-03-05 23:59:27 -0800

    ow

--- connect4player.py
+++ connect4player.py
@@ -43,12 +43,13 @@ class ComputerPlayer:
         column_major = [[row[column] for row in rack] for column in range(len(rack[0]))]
         # If the rack is under a certain dimension size then there's nothing that can be done but it should still play 
 
-        # play, minimax_score = self._minimax(column_major, self.DIFFICULTY, self.PLAYER_ID)
+        play, minimax_score = self._minimax(column_major, self.DIFFICULTY, self.PLAYER_ID)
         # play, minimax_score = self._minimax_alphabeta(column_major, self.DIFFICULTY, -self.INFINITY, self.INFINITY, self.PLAYER_ID)
 
+        # WIP
         time.sleep(0.5)  # pause purely for effect--real AIs shouldn't do this
         while True:
-            play = random.randrange(0, len(rack))
+            # play = random.randrange(0, len(rack))
             if column_major[play][-1] == 0:
                 return play
             
@@ -56,6 +57,7 @@ class ComputerPlayer:
     
     # Inspects windows of size 4 of diagonal elements from a 2D array.
     def _sliding_window(self,board):
+        # gets the values not the coords
         rows = len(board)
         columns = len(board[0])
         quartet = []
@@ -63,7 +65,6 @@ class ComputerPlayer:
         # print("vertical")
         for row in range(rows - self.CONNECT_WINDOW_LEN + 1):
             for col in range(columns):
-                # gets the values not the coords
                 window = []
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row + index_offset][col])
@@ -74,7 +75,6 @@ class ComputerPlayer:
         # print("horizontal")
         for row in range(rows):
             for col in range(columns - self.CONNECT_WINDOW_LEN + 1):
-                # gets the values not the coords
                 window = []
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row][col + index_offset])
@@ -84,7 +84,6 @@ class ComputerPlayer:
         # print("down-left diagonal") 
         for row in range(rows - self.CONNECT_WINDOW_LEN + 1):
             for col in range(columns - self.CONNECT_WINDOW_LEN + 1):
-                # gets the values not the coords
                 window = []
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row + index_offset][col  + index_offset])
@@ -94,7 +93,6 @@ class ComputerPlayer:
         # print("up-right diagonal")
         for row in range(rows - self.CONNECT_WINDOW_LEN + 1):
             for col in range(columns - (self.CONNECT_WINDOW_LEN - 1)):
-                # gets the values not the coords
                 window = []
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row + index_offset][col  - index_offset])
@@ -103,42 +101,42 @@ class ComputerPlayer:
         
         return quartet
     
-    def _evaluate_quartet(self, quartet, player_disc):
-        score = 0
-        opponent_disc = self.PLAYER2_DISC
-        if player_disc == self.PLAYER2_DISC:
-            opponent_disc = self.PLAYER1_DISC
-
-        # avoid multiple count() calls
-        player_disc_count = quartet.count(player_disc)
-        opponent_disc_count = quartet.count(opponent_disc)
-        empty_slot_count = quartet.count(self.EMPTY_SLOT)
-
-        if player_disc_count == 4:
-            score += self.INFINITY
-        elif player_disc_count == 3 and empty_slot_count == 1:
-            score += self.CONNECT3
-        elif player_disc_count == 2 and empty_slot_count == 2:
-            score += self.CONNECT2
-        elif player_disc_count == 1 and empty_slot_count == 3:
-            score += self.SOLO_DISC
-
-        if opponent_disc_count == 4:
-            score -= self.INFINITY
-        elif opponent_disc_count == 3 and empty_slot_count == 1:
-            score -= self.CONNECT3
-        elif opponent_disc_count == 2 and empty_slot_count == 2:
-            score -= self.CONNECT2
-        elif opponent_disc_count == 1 and empty_slot_count == 3:
-            score -= self.SOLO_DISC
-
-        return score
 
     def _evaluate(self,board):
         rows = len(board)
         columns = len(board[0])
         score = 0
+        column_choice = 0 # need to find best column
 
+        def evaluate_quartet(self, quartet, player_disc):
+            opponent_disc = self.PLAYER2_DISC
+            if player_disc == self.PLAYER2_DISC:
+                opponent_disc = self.PLAYER1_DISC
+
+            player_disc_count = quartet.count(player_disc)
+            opponent_disc_count = quartet.count(opponent_disc)
+            empty_slot_count = quartet.count(self.EMPTY_SLOT)
+
+            if player_disc_count == 4:
+                score += self.INFINITY
+            elif player_disc_count == 3 and empty_slot_count == 1:
+                score += self.CONNECT3
+            elif player_disc_count == 2 and empty_slot_count == 2:
+                score += self.CONNECT2
+            elif player_disc_count == 1 and empty_slot_count == 3:
+                score += self.SOLO_DISC
+
+            if opponent_disc_count == 4:
+                score -= self.INFINITY
+            elif opponent_disc_count == 3 and empty_slot_count == 1:
+                score -= self.CONNECT3
+            elif opponent_disc_count == 2 and empty_slot_count == 2:
+                score -= self.CONNECT2
+            elif opponent_disc_count == 1 and empty_slot_count == 3:
+                score -= self.SOLO_DISC
+
+            return score
+    
         # print("_evaluate vertical")
         for row in range(rows - self.CONNECT_WINDOW_LEN + 1):
             for col in range(columns):
@@ -146,6 +144,7 @@ class ComputerPlayer:
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row + index_offset][col])
                 score += self._evaluate_quartet(window, self.PLAYER_ID)
+                # if score +INFINTY then column_choice = col
 
         # print("_evaluate horizontal")
         for row in range(rows):
@@ -154,6 +153,7 @@ class ComputerPlayer:
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row][col + index_offset])
                 score += self._evaluate_quartet(window, self.PLAYER_ID)
+                # find col ?
 
         # print("_evaluate down-left diagonal") 
         for row in range(rows - self.CONNECT_WINDOW_LEN + 1):
@@ -162,6 +162,7 @@ class ComputerPlayer:
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row + index_offset][col  + index_offset])
                 score += self._evaluate_quartet(window, self.PLAYER_ID)
+                # find col ?
 
         # print("_evaluate up-right diagonal")
         for row in range(rows - self.CONNECT_WINDOW_LEN + 1):
@@ -170,41 +171,35 @@ class ComputerPlayer:
                 for index_offset in range(self.CONNECT_WINDOW_LEN):
                     window.append(board[row + index_offset][col  - index_offset])
                 score += self._evaluate_quartet(window, self.PLAYER_ID)
+                # find col ?
 
 
     def _minimax(self, board, depth, player):
-        if depth == 0:
-            # return evaluation_function(state)
-            pass
+        if depth == 0: # or is terminal
+            return self._evaluate(board)
         legal_moves = self._get_legal_moves(board)
 
         if player == self.PLAYER_ID:
             best_value = -self.INFINITY
-            column_choice = random.choice(legal_moves) # !!! shouldn't be a random choice
+            column_choice = random.choice(legal_moves) # temporary random choice
             for column in legal_moves:
-                # row = get_next_open_row(board, col)
                 board_copy = board.copy()
-                # drop_piece(board_copy, row, col, AI_PIECE)
                 new_state = self.pick_move(board_copy)
                 value = self._minimax(new_state, depth - 1, self.PLAYER_ID)
                 if best_value > value:
                     value = best_value
                     column_choice = column
-                # best_value = max(best_value, value)
             return column_choice, best_value
         else:
             best_value = self.INFINITY
-            column_choice = random.choice(legal_moves) # !!! shouldn't be a random choice
+            column_choice = random.choice(legal_moves) # temporary random choice
             for column in legal_moves:
-                # row = get_next_open_row(board, col)
                 board_copy = board.copy()
-                # drop_piece(board_copy, row, col, AI_PIECE)
                 new_state = self.pick_move(board_copy)
                 value = self._minimax(new_state, depth - 1, self.PLAYER_ID)
                 if best_value < value:
                     value = best_value
                     column_choice = column
-                # best_value = max(best_value, value)
             return column_choice, best_value
 
     
